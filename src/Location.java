ebd733cc6c8e7c551cb403feb79d40aff8c43064
Dan <dan.blazek07@gmail.com>
2025-03-04 19:53:05 +0100

    world now exist

---
+++ src/Location.java
@@ -0,0 +1,23 @@
+public class Location {
+    private String name;
+    private int id;
+    private int[] locations;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int[] getLocations() {
+        return locations;
+    }
+
+    public Location(int id, String name, int[] locations) {
+        this.name = name;
+        this.id = id;
+        this.locations = locations;
+    }
+}
---
+++ src/World.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class World {
+    private HashMap<Integer, Location> world = new HashMap<>();
+    private Location location;
+
+    public World() {
+        loadLocation();
+        location = world.get(1);
+    }
+
+    public String getLocation() {
+        return location.getName();
+    }
+
+    public int getId(){
+        return location.getId();
+    }
+
+    public boolean loadLocation(){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/rooms/rooms.txt"));
+            String line = br.readLine();
+            while (line != null){
+                String[] text = line.split("-");
+                Location l = new Location(Integer.parseInt(text[0]), text[1], locations(text[2]));
+            }
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    private int[] locations(String locations){
+        String[] location = locations.split(",");
+        int[] locationss = new int[locations.length()];
+        for (int i = 0; i < locations.length(); i++) {
+            locationss[i] = Integer.parseInt(location[i]);
+        }
+        return locationss;
+    }
+}
