68969593e5d10f9925e512830fedad0f2c37cab9
Dan <dan.blazek07@gmail.com>
2025-03-05 11:53:48 +0100

    commands added, beginning the talk with people in the room

--- src/commands/Use.java
+++ src/commands/Use.java
@@ -1,4 +1,24 @@
 package commands;
 
-public class Use {
+import world.*;
+
+public class Use extends Command{
+    private boolean hasKey = false;
+    private boolean canUnlockDoor = false;
+    private Location location;
+    @Override
+    public String execute() {
+        if (hasKey && canUnlockDoor){
+            return "door to the room" + location.getId() + "unlocked";
+        } else if (hasKey) {
+            return "as hard as u tried, the door didnt open... try a different one";
+        } else {
+            return "u cant do anything";
+        }
+    }
+
+    @Override
+    public boolean exit() {
+        return false;
+    }
 }
